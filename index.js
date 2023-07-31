9e1ce49af25bc2ff9f780e6ccaac92cc9c092b15
Harish <harish.sathuluri@gmail.com>
2023-07-31 11:23:53 +0000

    super over league-reactjs

---
+++ index.js
@@ -0,0 +1,10 @@
+const element = (
+//   Write your code here.
+<div className="bg-color">
+<h1 className="heading1"> Super Over League </h1>
+<img className="image1" src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"/>
+<img className="image1" src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+</div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
